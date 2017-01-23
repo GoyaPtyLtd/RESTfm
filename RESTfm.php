2321ea3d2dca91da52434a946909a31e536ebbd0
Gavin Stewart <gav-@users.noreply.github.com>
2017-01-23 16:28:55 +1100

    Enable dev level debugging.

--- RESTfm.php
+++ RESTfm.php
@@ -26,8 +26,8 @@
 $startTimeUs = microtime(TRUE);
 
 // Ensure E_STRICT is removed for PHP 5.4+
-error_reporting(E_ALL & ~E_NOTICE & ~E_STRICT & ~E_DEPRECATED);
-//error_reporting(E_ALL & ~E_STRICT);   // Dev. level reporting
+//error_reporting(E_ALL & ~E_NOTICE & ~E_STRICT & ~E_DEPRECATED);
+error_reporting(E_ALL & ~E_STRICT);   // Dev. level reporting
 
 // x-debug's html error output makes CLI debugging with cURL a problem.
 ini_set('html_errors', FALSE);
