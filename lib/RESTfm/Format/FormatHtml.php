fc73295bb0baa97cf697fb193016a09d02923991
Gavin Stewart <gav-@users.noreply.github.com>
2017-05-05 11:00:00 +1000

    Update logo with correct case.

--- lib/RESTfm/Format/FormatHtml.php
+++ lib/RESTfm/Format/FormatHtml.php
@@ -49,7 +49,7 @@ class FormatHtml implements FormatInterface {
         $str .= '<link type="text/css" rel="stylesheet" href="' . \RESTfm\Config::getVar('settings', 'baseURI') . '/css/RESTfm.css">'."\n";
         $str .= "</head><body>\n";
         $str .= '<div id="logo">' .
-                '<a target="_blank" href="http://www.restfm.com"><img width="106" height="36" src="' . \RESTfm\Config::getVar('settings', 'baseURI') . '/css/RESTfm.logo.png" alt="RESTfm logo"></a>' .
+                '<a target="_blank" href="http://www.restfm.com"><img width="106" height="33" src="' . \RESTfm\Config::getVar('settings', 'baseURI') . '/css/RESTfm.logo.png" alt="RESTfm logo"></a>' .
                 '<span>' . \RESTfm\Version::getRelease() . '</span>' .
             '</div>' . "\n";
 
