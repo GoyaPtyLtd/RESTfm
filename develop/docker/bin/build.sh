abac454937533d6f9172cab86b2a695d9ca3003d
Gavin Stewart <gav-@users.noreply.github.com>
2023-11-24 15:10:08 +1100

    FMS docker development env WIP

---
+++ develop/docker/bin/build.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+# Build docker images for RESTfm and FileMaker Server
+
+# Identify our exact location and ensure we are in the correct directory.
+RELDIR=$(dirname "$0")
+cd "${RELDIR}"/.. || exit 1
+
+# Load common environment vars
+source .env
+
+# Sanity checks, make sure needed files exist
+echo
+echo "** Checking for required files"
+REQUIRED_FILES=(
+    ".env"
+    "./files/fms/installer/Assisted Install.txt"
+    "./files/fms/installer/LicenseCert.fmcert"
+)
+MISSING_COUNT=0
+for filename in "${REQUIRED_FILES[@]}"; do
+    if test -f "$filename"; then
+        echo "Found: $filename"
+    else
+        echo "Missing: $filename"
+        MISSING_COUNT=$(($MISSING_COUNT + 1))
+    fi
+done
+if [[ $MISSING_COUNT -gt 1 ]]; then
+    echo "!! Error required file(s) missing"
+    exit 1
+fi
+echo
+
+echo "** Checking for filemaker-server v${FMS_VERSION} .deb file"
+FMS_INSTALLER_COUNT=0
+for filename in "./files/fms/installer/filemaker-server-${FMS_VERSION}."*".deb"; do
+    echo "Found: $filename"
+    FMS_INSTALLER_COUNT=$(($FMS_INSTALLER_COUNT + 1))
+done
+if [[ $FMS_INSTALLER_COUNT -eq 0 ]]; then
+    echo "!! Error required file missing"
+    exit 1
+fi
+if [[ $FMS_INSTALLER_COUNT -gt 1 ]]; then
+    echo "!! Error $FMS_INSTALLER_COUNT matching .deb files for v${FMS_VERSION}, there can be only one."
+    exit 1
+fi
+echo
+
+# Identify package dependencies in filemaker-server .deb
+echo "** Identify filemaker-server .deb package dependencies"
+FMS_PACKAGE_DEPS=./files/fms/installer/fms-package-deps.txt
+dpkg -I "./files/fms/installer/filemaker-server-${FMS_VERSION}."*".deb" | \
+    grep Depends: | sed -e 's/ Depends: //' -e 's/, / /g' > \
+    "${FMS_PACKAGE_DEPS}"
+cat < "${FMS_PACKAGE_DEPS}"
+echo
+
+# Build it!
+echo "** Building fms docker image"
+docker-compose build --pull fms
--- develop/docker/files/fms/installer/README.txt
+++ develop/docker/files/fms/installer/README.txt
@@ -1,6 +1,16 @@
-This directory will be mounted as a volume in the container at /fms-install
-
 This directory should contain:
-- 'filemaker-server-xx.xx.xx.xx-amd64.deb'
+- 'filemaker-server-{major}.{minor}.xx.yy-{arch}.deb'
 - 'Assisted Install.txt'
 - 'LicenseCert.fmcert'
+
+Note: Ensure there is not more than one major.minor version of the
+      filemaker-server .deb package here!
+
+      This is ok (version 20.2 and 20.3 are different):
+        filemaker-server-20.2.1.19-amd64.deb
+        filemaker-server-20.3.1.31.19-amd64.deb
+        (20.2 and 20.3 are different here)
+
+      This is NOT ok (version 20.3 is common to both):
+        filemaker-server-20.3.0.31-amd64.deb
+        filemaker-server-20.3.1.31-amd64.deb
