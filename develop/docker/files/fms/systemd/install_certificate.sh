2b56f915555ff0a6154dbf2cc24fdf9904cff704
Gavin Stewart <gav-@users.noreply.github.com>
2023-12-23 23:10:11 +1100

    dev docker fms - fix cert install for slow systems
    - tested on raspberry pi 4 with arm64 FMS build

--- develop/docker/files/fms/systemd/install_certificate.sh
+++ develop/docker/files/fms/systemd/install_certificate.sh
@@ -15,10 +15,6 @@ if [[ -e "${READY_FILE}" ]]; then
     exit 0
 fi
 
-# Race with fmshelper startup
-# FIXME: This needs to be a polling loop
-sleep 1
-
 # Pull the FMS Admin Console user and password from Assisted Install.txt
 FMS_AC_USER="$(grep "Admin Console User=" "/fms/Assisted Install.txt")"
 FMS_AC_USER="${FMS_AC_USER#*=}"
@@ -26,10 +22,47 @@ FMS_AC_PASS="$(grep "Admin Console Password=" "/fms/Assisted Install.txt")"
 FMS_AC_PASS="${FMS_AC_PASS#*=}"
 
 echo " ++ Installing SSL/TLS certificate ..."
-fmsadmin certificate import -y -u "${FMS_AC_USER}" -p "${FMS_AC_PASS}" \
-    /certs/certificate.pem \
-    --keyfile /certs/certificate.key  \
-    --keyfilepass "$(cat /certs/certificate.pass)"
+fmsadmin_code=-999
+attempt_count=1
+while [[ $fmsadmin_code -ne 0 && $attempt_count -le 10 ]]; do
+
+    fmsadmin_output=$(fmsadmin certificate import -y -u "${FMS_AC_USER}" -p "${FMS_AC_PASS}" \
+        /certs/certificate.pem \
+        --keyfile /certs/certificate.key  \
+        --keyfilepass "$(cat /certs/certificate.pass)" 2>&1)
+    fmsadmin_code=$?
+
+    if [[ $fmsadmin_code -ne 0 ]]; then
+        echo "  ++ fmsadmin certificate import failed attempt: ${attempt_count}"
+
+        if [[ "$fmsadmin_output" =~ .*"Error: 9"|"10502".* ]]; then
+            # Error: 9 (Access denied)
+            # Error: 10502 (Host unreachable)
+            #
+            # These are common errors on slower systems while fmshelper
+            # starts up, so we ignore and try again. Don't show the error.
+            :
+        else
+            # Output this fmsadmin error message though, and try again.
+            echo "  !! Unexpected error: $fmsadmin_code: $fmsadmin_output"
+        fi
+
+        ((attempt_count+=1))
+        sleep 2
+    else
+        # Success!
+        echo "${fmsadmin_output}"
+        break
+    fi
+
+done
+
+if [[ $fmsadmin_code -ne 0 ]]; then
+    echo " !! Failed to install SSL/TLS certificate, last error was:"
+    echo "$fmsadmin_output"
+    exit 1
+fi
+
 echo " ++ ... done"
 
 echo " ++ Restarting fmshelper"
